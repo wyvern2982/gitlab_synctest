61e389a9fa8a668a785145e673c3edf159af6554
EnCheng Lin <wyvern2982@gmail.com>
2020-05-26 23:05:58 +0800

    Init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitlab_synctest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
